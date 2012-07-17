8687ab0e9060f54394000a1ab84bd32803404fd4
Magnus Feuer <magnus@feuerlabs.com>
2012-07-16 21:15:20 -0700

    Added MPL2.0 license

--- c_src/gpio_driver.c
+++ c_src/gpio_driver.c
@@ -1,8 +1,12 @@
-/*! @file gpio_driver.c
-
-    Copyright (C) 2011, Feuerlabs, Inc. All rights reserved.
-    Redistribution and use in any form, with or without modification, is strictly prohibited.
-*/
+/****** BEGIN COPYRIGHT *******************************************************
+ *
+ * Copyright (C) 2012 Feuerlabs, Inc. All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ ****** END COPYRIGHT ********************************************************/
 
 #include "erl_driver.h"
 #include <sys/types.h>
